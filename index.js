f342714ad498e23ace70b85b72b10847b4dafd42
Harshit Bagewadi <98148017+Harsh-Repository@users.noreply.github.com>
2023-06-13 20:54:00 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -51,11 +51,11 @@ ScrollReveal().reveal(
   `.home-img, .service-container, .portfolio-box, .contact .social-media`,
   { origin: `bottom` }
 );
-ScrollReveal().reveal(`.home-content h1, .certifications div h3`, {
+ScrollReveal().reveal(`.home-content h1, .certifications div h3, .skill-left`, {
   origin: `left`,
 });
 ScrollReveal().reveal(
-  `.home-content p, .about-content, .certifications-media`,
+  `.home-content p, .about-content, .certifications-media, .skill-right`,
   { origin: `right` }
 );
 
@@ -66,4 +66,24 @@ const typed = new Typed(`.multiple-text`, {
   backSpeed: 100,
   backDelay: 1000,
   loop: true,
+});
+
+/*================ Circle skills ================*/
+const circles = document.querySelectorAll(`.circle`);
+circles.forEach((elem) => {
+  var dots = elem.getAttribute(`data-dots`);
+  var marked = elem.getAttribute(`data-percent`);
+  var percent = Math.floor((dots * marked) / 100);
+  var points = "";
+  var rotate = 360 / dots;
+
+  for (let i = 0; i < dots; i++) {
+    points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+  }
+  elem.innerHTML = points;
+
+  const pointsMarked = elem.querySelectorAll(`.points`);
+  for (let i = 0; i < percent; i++) {
+    pointsMarked[i].classList.add(`marked`);
+  }
 });
